7847471240f5bda1e61b1bc2b1ac0f0ec2643caa
BitsFactor <bitsfactorcom@gmail.com>
2026-05-01 11:48:54 -0700

    dev-push: 2026-05-01 11:48:54

--- src/ssl_proxy_controller/db.py
+++ src/ssl_proxy_controller/db.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 from contextlib import contextmanager
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Iterator
 
